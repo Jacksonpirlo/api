1970ac9d487def6a9ccd92c1b091963da1eb4b29
Jacksonpirlo <jacksonflorezp@gmail.com>
2025-09-04 16:10:21 -0500

    add singleCard

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,8 +4,7 @@ import axios from "axios";
 
 const Cards = ({section}) => {
     const [persons, setPerson] = useState([]);
-    const [moreAbout, setMoreAbout] = useState(false);
-    const [selectedCard, setSelectedCard] = useState(null);
+    const [moreAboutById, SetMoreAboutById] = useState(null);
     useEffect(() => {
         const getUsers = async () => {
             const res = await axios.get('https://rickandmortyapi.com/api/character');
@@ -20,8 +19,9 @@ const Cards = ({section}) => {
             <h1 className="m-12 text-amber-50 font-bold text-4xl">{section}</h1>
             <article className="flex flex-wrap justify-center items-center">
 
-                {persons.map((person) => {
+                {moreAboutById === null ? persons.map((person) => {
                     return (
+
                         <div key={person.id} className= "flex gap-4 flex-col items-start bg-green-900 shadow-lg shadow-green-500 m-3.5 text-white rounded-2xl max-w-2xl sm:bg-green-950 p-5 hover:shadow-green-300">
                             <h1 className="font-bold">{person.name}</h1>
                             <img className="rounded-2xl" src={person.image} alt={person.name} />
@@ -30,13 +30,20 @@ const Cards = ({section}) => {
                             <p>{person.gender}</p>
                             <p>{person.origin.name}</p>
                             <p>{person.location.name}</p>
-                            <button onClick={() => setMoreAbout(!moreAbout)}>More About</button>
-                            {moreAbout && (
-                                <MoreAbout name={person.name} status={person.status} species={person.species} gender={person.gender} origin={person.origin} location={person.location} />
-                            )}
+                            <button onClick={() => {SetMoreAboutById(person.id);}}>More About</button>
                         </div>
+
                     )
-                })}
+                }) 
+            : persons.filter((person) => person.id === moreAboutById).map((person) => {
+                return (
+
+                        <>
+                        <MoreAbout id={person.id} name={person.name} img={person.image} status={person.status} species={person.species} gender={person.gender} origin={person.origin} location={person.location} />
+                        </>
+                    )
+            })
+            }
             </article>
         </section>
 
--- src/components/MoreAbout.jsx
+++ src/components/MoreAbout.jsx
@@ -1,15 +1,15 @@
-const MoreAbout = ({name, status, species, gender, origin, location}) => {
+const MoreAbout = ({id, img, name, status, species, gender, origin, location}) => {
 
     return (
-        <>
-
+        <div key={id} className="flex gap-4 flex-col items-start bg-green-900 shadow-lg shadow-green-500 m-3.5 text-white rounded-2xl max-w-2xl sm:bg-green-950 p-5 hover:shadow-green-300">
         <h2>Name: {name}</h2>
+        <img src={img} alt={name} />
         <p>Status: {status}</p>
         <p>Species: {species}</p>
         <p>Gender: {gender}</p>
         <p>Origin: {origin.name}</p>
         <p>Location: {location.name}</p>
-        </>
+        </div>
     )
 }
 
